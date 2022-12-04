721003f90f4369cff1fbd7c7866fe708a1df349b
jamesahnking <jamesahnking@users.noreply.github.com>
2022-12-03 19:32:25 -0500

    Added last of app scaffolding

--- index.mjs
+++ index.mjs
@@ -1,22 +1,37 @@
 import { loadStdlib } from "@reach-sh/stdlib";
 import * as backend from "./build/index.main.mjs";
 import { ask } from '@reach-sh/stdlib';
+
 // Load the standard library
 const stdlib = loadStdlib(process.env);
 
 // Starting Balance For Participants
-const stargingBalance = stdlib.parseCurrency(100);
+const startingBalance = stdlib.parseCurrency(100);
 
 // Create and fund (2) accounts A/B
-const [accA, accB] = await stdlib.newTestAccounts(2, stargingBalance);
+const [accA, accB] = await stdlib.newTestAccounts(2, startingBalance);
 console.log('Hello account A and Account B');
 
 // App message 
-console.log('Hello, A and B!');
+console.log('Launching..');
 
 // Add and attach to the contract 
 const ctcA = accA.contract(backend);
 const ctcB = accB.contract(backend, ctcA.getInfo());
 
-console.log('Staring backends.....');
+console.log('Starting backends.....');
 
+// Resolve several promises at once 
+await Promise.all([
+    // send contract to backend 
+    backend.A(ctcA,{
+        ...stdlib.hasRandom,
+    }),
+    
+    backend.B(ctcB,{
+        ...stdlib.hasRandom,
+
+    })
+]);
+
+console.log('Goodby, A and B account');
